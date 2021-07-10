7f16bd7e08da15c9559977fb6fbfcdd0ca970bf5
horxyz <cjh_777@126.com>
2021-07-10 21:31:25 +0800

    多模块操作

---
+++ admin/src/main/java/com/nit/admin/controller/Index.java
@@ -0,0 +1,12 @@
+package com.nit.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class Index {
+    @GetMapping("")
+    String index(){return "index";}
+}
---
+++ main/src/main/java/com/nit/main/MainApplication.java
@@ -0,0 +1,13 @@
+package com.nit.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.nit"})
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
